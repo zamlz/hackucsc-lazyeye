f909536e229212828415acb1b29195f416de578c
CarpeSwag <james@garbagnati.com>
2017-01-21 02:47:49 -0800

    Fix eye detection most of the time (Has issues with glasses and looking downwards).

--- source/find_face.py
+++ source/find_face.py
@@ -28,7 +28,13 @@ def detectFacialFeatures(imgPath, imgName):
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
+        eyes = eye_cascade.detectMultiScale(
+            roi_gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(100, 100),
+            maxSize=(180, 180)
+        )
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
     cv2.imshow(imgName,img)
