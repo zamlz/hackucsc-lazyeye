ebb13eed5882e9da8338cbf8737c37e6f96e4f19
CarpeSwag <james@garbagnati.com>
2017-01-22 01:50:37 -0800

    Got a toggleable icon in the systems tray.

---
+++ source/const.py
@@ -0,0 +1,13 @@
+# Tray Constants
+TRAY_ICON = [
+    "images/logo_tray_inactive.png",
+    "images/logo_tray_active.png"
+]
+TRAY_TOOLTIP = [
+    "Who's Lazy? Not Eye! (Inactive - Press to activate)",
+    "Who's Lazy? Not Eye! (Active - Press to disable)"
+]
+
+# Camera Flags
+CAMERA_INACTIVE = 0
+CAMERA_ACTIVE = 1
--- source/lazy.py
+++ source/lazy.py
@@ -1,43 +1,42 @@
 import sys
 import find_face
+import const
 from PyQt5 import QtCore, QtGui, QtWidgets
-
-# Tray Constants
-TRAY_ICON = [
-    "images/logo_tray_inactive.png",
-    "images/logo_tray_active.png"
-]
-TRAY_TOOLTIP = [
-    "Who's Lazy? Not Eye! (Inactive - Press to activate)",
-    "Who's Lazy? Not Eye! (Active - Press to disable)"
-]
-
-# Camera Flags
-CAMERA_INACTIVE = 0
-CAMERA_ACTIVE = 1
-
-# Globals
-cameraState = CAMERA_ACTIVE
 
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
     def __init__(self, icon, parent=None):
         QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
+
+        self.cameraState = const.CAMERA_ACTIVE
+        self.setToolTip(const.TRAY_TOOLTIP[const.CAMERA_ACTIVE])
+
         self.menu = QtWidgets.QMenu(parent)
         exitAction = self.menu.addAction("Exit")
         exitAction.setStatusTip('Exit Application')
         exitAction.triggered.connect(sys.exit)
         self.setContextMenu(self.menu)
-        
+
+
+    def toggle(self):
+        # Toggle camera state and switch icon and tooltip
+        self.cameraState = (self.cameraState + 1) % 2
+        self.setToolTip(const.TRAY_TOOLTIP[self.cameraState])
+        icon = QtGui.QIcon(QtGui.QPixmap(const.TRAY_ICON[self.cameraState]))
+        self.setIcon(icon)
+
+
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     style = app.style()
-    icon = QtGui.QIcon(QtGui.QPixmap(TRAY_ICON[CAMERA_ACTIVE]))
+    icon = QtGui.QIcon(QtGui.QPixmap(const.TRAY_ICON[const.CAMERA_ACTIVE]))
     trayIcon = SystemTrayIcon(icon)
-    trayIcon.setToolTip(TRAY_TOOLTIP[CAMERA_ACTIVE])
+    trayIcon.activated.connect(trayIcon.toggle)
     trayIcon.show()
     find_face.main()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
